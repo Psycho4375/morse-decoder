6af41634075d21f3f48aa85ccc721352736464e5
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-07 16:46:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let str = ""
+    let arr = []
+    for (let i = 0; i < expr.length; i +=10){
+        arr.push(expr.substr(i, 10))
+    }
+    arr.forEach(item =>{
+        let str2 = ""
+        for (let i = 0; i < item.length; i += 2){
+            if (item.substr(i, 2) == '10'){
+                str2 = str2.concat('.')
+            } else if (item.substr(i, 2) == '11'){
+                str2 = str2.concat('-')
+            } else if (item.substr(i, 2) == '**'){
+                str2 = str2.concat(' ')
+                i += 8
+            } 
+
+        };
+        return arr.push(str2)
+    });
+    arr.forEach(item => {
+        if (MORSE_TABLE.hasOwnProperty(item)){
+            str = str.concat(MORSE_TABLE[item])
+        }
+    })
+    return str
+    
 }
 
 module.exports = {
